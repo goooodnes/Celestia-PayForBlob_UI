bedbc2dc18b45eaf7893fd11ad6a1ff531aaba9c
Alexey <77731459+goooodnes@users.noreply.github.com>
2023-05-14 00:58:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+	<title>Celestia | PayForBlob</title>
+  <link rel='stylesheet' href='//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'><link rel="stylesheet" href="style.css">
+
+</head>2
+<body>
+<body>
+    <div class="info" style="text-align: center;">
+		<img src="/assets/img/celestia-logo.svg" alt="Celestia" width="170">
+		<a href="https://stakevillage.net" target="_blank"><p> Made with <i class="fa fa-heart"></i> by Stake Village </p></a>
+	</div>
+  
+<div class="content">
+	<div class="container">
+		<img class="bg-img" src="./assets/img/bg2.jpeg" alt="">
+			<div class="menu">
+				<a href="#submit" class="btn-submit"><h2>Send transaction</h2></a>
+
+			</div>
+			<div class="connexion">
+				<div class="contact-form">
+					<form action="./assets/submit.php" method="post" id="form">
+
+					<label>Data</label>
+					<input name="data" placeholder="" type="text">
+
+					<label>Gas limit:</label>
+					<input name="gas" placeholder="" type="number" value="80000" min="1">
+
+					<label>Fee:</label>
+					<input name="fee" placeholder="" type="number" value="2000" min="1">
+
+					<br>
+
+					<input class="submit" value="Send data" type="submit">
+					</form>
+				</div>
+				
+			</div>
+
+	</div>
+
+</div>
+
+</body>
+
+</body>
+</html>
